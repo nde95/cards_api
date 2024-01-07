03159c2b69f5b875e15107ae7d4ab07de4447f5d
nde95 <ndennison268@gmail.com>
2024-01-07 16:16:27 -0500

    testing sorting methods

--- card_test.go
+++ card_test.go
@@ -28,3 +28,19 @@ func TestNew (t *testing.T) {
 		t.Error("Wrong amount of cards created")
 	}
 }
+
+func TestDefaultSort (t *testing.T) {
+	cards := NewCard(DefaultSort)
+	expected := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != expected {
+		t.Error("Expected Ace of Spades as first result, instead got:", cards[0])
+	}
+}
+
+func TestSort (t *testing.T) {
+	cards := NewCard(Sort(Less))
+	expected := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != expected {
+		t.Error("Expected Ace of Spades as first result, instead got:", cards[0])
+	}
+}
